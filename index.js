3479324d9c173b5e12efcd56204c01aff5a10063
CarlosRomeroV <cromveloso@gmail.com>
2025-04-23 10:56:44 +0200

    cambio de CORS en index.js

--- index.js
+++ index.js
@@ -9,22 +9,34 @@ const spotifyRoutes = require("./routes/spotify");
 const geniusRoutes = require("./routes/genius");
 const userRoutes = require("./routes/user");
 
-
 const app = express();
-const PORT = 8888;
+const PORT = process.env.PORT || 8888;
 
+// Lista de orígenes permitidos
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://coldtify.vercel.app"
+];
+
+// Middleware CORS con validación dinámica
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Origen no permitido por CORS"));
+    }
+  },
   credentials: true
 }));
+
 app.use(express.json());
+
 app.use("/user", userRoutes);
 app.use("/", authRoutes);
 app.use("/spotify", spotifyRoutes);
 app.use("/genius", geniusRoutes);
 
-
-
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://127.0.0.1:${PORT}`);
+  console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
 });
